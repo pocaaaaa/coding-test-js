a789e6fb59fefe68d34db7fd5d338a32f50d2ab3
HYUNJUNG <space90220@gmail.com>
2023-03-19 16:59:52 +0900

    20230319_level1_옹알이(2)

---
+++ level1/옹알이(2).js
@@ -0,0 +1,29 @@
+/*
+    [문제]
+    머쓱이는 태어난 지 11개월 된 조카를 돌보고 있습니다. 
+    조카는 아직 "aya", "ye", "woo", "ma" 네 가지 발음과 네 가지 발음을 조합해서 만들 수 있는 발음밖에 하지 못하고 연속해서 같은 발음을 하는 것을 어려워합니다. 
+    문자열 배열 babbling이 매개변수로 주어질 때, 머쓱이의 조카가 발음할 수 있는 단어의 개수를 return하도록 solution 함수를 완성해주세요.
+
+    [제한사항]
+     1. 1 ≤ babbling의 길이 ≤ 100
+     2. 1 ≤ babbling[i]의 길이 ≤ 30
+     3. 문자열은 알파벳 소문자로만 이루어져 있습니다.
+*/
+
+const solution = (babbling) => {
+  return babbling.filter((item) => {
+    let prevTarget = "";
+    item = item.replace(/aya|ye|woo|ma/gi, (target) => {
+      if (prevTarget === target) {
+        return target;
+      } else {
+        prevTarget = target;
+        return "";
+      }
+    });
+    return !item;
+  }).length;
+};
+
+console.log(solution(["aya", "yee", "u", "maa"])); // 1
+console.log(solution(["ayaye", "uuu", "yeye", "yemawoo", "ayaayaa"])); // 2
